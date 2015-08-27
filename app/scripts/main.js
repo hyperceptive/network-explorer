4973319fd348c35130d8fcf3f68beb3309078333
Matt Hill <matt@codebones.com>
2015-08-27 16:01:16 -0700

    Better Visual

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,78 +1,126 @@
-console.log('yo!');
 
-var width = 960,
-    height = 500;
+var width = 1400,
+    height = 900;
 
-var color = d3.scale.category20();
+var color = d3.scale.category10();
 
+
+
+//Construct a force layout...
 var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(80)
-    .size([width, height]);
+    .size([width, height])
+    //.linkDistance(20)
+    /*
+    .linkStrength(function(d) {
+      console.log(d.value);
+      return 0.5;
+    }) */
+    .gravity(0.9)
+    .charge(-300)
+    .chargeDistance(600)
+    ;
+
+
+/* defaults:
+    .linkStrength(0.1)  //or 1?
+    .friction(0.9)
+    .linkDistance(20)
+    .charge(-30)
+    .gravity(0.1)
+    .theta(0.8)
+    .alpha(0.1)
+
+*/
+
+
+
+
+var drag = force.drag()
+      .on('dragstart', dragstart);
+
+
+
+function dragstart(d) {
+  var dragNode = d3.select(this);
+  dragNode.classed('fixed', d.fixed = true);
+  dragNode
+    .select('.label')
+    .attr('opacity', 1.0);
+
+  //fish: What about the nodes connected???  Also show their labels.
+
+}
+
+
+function dblclick(d) {
+  var dragNode = d3.select(this);
+  dragNode.classed('fixed', d.fixed = false);
+  dragNode
+    .select('.label')
+    .attr('opacity', 0);
+}
+
+
+
+function onMouseOver(d) {
+  //Loop over labels and check if there is an edge from this node to/from that node.
+  svg.selectAll('.label')
+    .attr('opacity', function(d2) {
+      if(d.name === d2.name || typeof d2.fixed !== 'undefined') {
+        return 1.0;
+      }
+      else if(d.group === 1) {
+        if(graph.getEdge(d.name, d2.name)) {
+          return 1.0;
+        }
+      }
+      else if(d.group === 2) {
+        if(graph.getEdge(d2.name, d.name)) {
+          return 1.0;
+        }
+      }
+
+      return 0.0;
+    });
+}
+
+
+function onMouseOut(d) {
+  //Loop over labels and check if there is an edge from this node to/from that node.
+  svg.selectAll('.label')
+    .attr('opacity', function(d2) {
+      if(typeof d2.fixed !== 'undefined') {
+        return 1.0;
+      }
+
+      return 0.0;
+    });
+}
+
+
+
+
 
 var svg = d3.select('body').append('svg')
     .attr('width', width)
     .attr('height', height);
 
 
-/*
-d3.json('data/miserables.json', function(error, graph) {
-  if (error) throw error;
-
-  force
-      .nodes(graph.nodes)
-      .links(graph.links)
-      .start();
-
-  var link = svg.selectAll('.link')
-      .data(graph.links)
-    .enter().append('line')
-      .attr('class', 'link')
-      .style('stroke-width', function(d) { return Math.sqrt(d.value); });
-
-  var node = svg.selectAll('.node')
-      .data(graph.nodes)
-    .enter().append('circle')
-      .attr('class', 'node')
-      .attr('r', 5)
-      .style('fill', function(d) { return color(d.group); })
-      .call(force.drag);
-
-  node.append('title')
-      .text(function(d) { return d.name; });
-
-  force.on('tick', function() {
-    link.attr('x1', function(d) { return d.source.x; })
-        .attr('y1', function(d) { return d.source.y; })
-        .attr('x2', function(d) { return d.target.x; })
-        .attr('y2', function(d) { return d.target.y; });
-
-    node.attr('cx', function(d) { return d.x; })
-        .attr('cy', function(d) { return d.y; });
-  });
-});
-*/
-
-
 d3.json('data/lobbyistsContacts.json', function(error, lobbyistsContacts) {
   if (error) throw error;
 
-  console.log('-------------- Lobbyists Contacts * double gag* --------------'); //fish
-  console.log(lobbyistsContacts.meta.view.columns);
-
   var columns = lobbyistsContacts.meta.view.columns;
 
-  //TODO: fish: Create the structures needed by d3 force layout.
+  //Create the structures needed by d3 force layout.
   //
-  //  Maybe we can just create the Graph data structure and use (new) methods
-  //  of the Graph class to export into d3 format for force layout.
+  //  TODO: Maybe move this code to the Graph data structure and use (new) methods
+  //        of the Graph class to export into d3 format for force layout.
   //
 
-  //{ name: "Myriel", group: 1 }
   var nodes = [];
-
-  //{ source: 1, target: 0, value: 1 }
   var links = [];
+
+  var foci = [{x: 200, y: 250}, {x: 500, y: 250}];
 
   graph = new Graph();
 
@@ -86,11 +134,11 @@ d3.json('data/lobbyistsContacts.json', function(error, lobbyistsContacts) {
     }, {});
 
     //Nodes for:
-    //Lobbyist: "Yaki, Michael"
-    //Lobbyist_Client: "Barneys New York"
-    //Lobbyist_Firm: "Michael Yaki"
-    //Official: "Yuen, Janis"
-    //Official_Department: "Municipal Transportation Agency"
+    //Lobbyist: 'Yaki, Michael'
+    //Lobbyist_Client: 'Barneys New York'
+    //Lobbyist_Firm: 'Michael Yaki'
+    //Official: 'Yuen, Janis'
+    //Official_Department: 'Municipal Transportation Agency'
 
     // -- Lobbyist
     // -- Lobbyist Firm
@@ -98,7 +146,6 @@ d3.json('data/lobbyistsContacts.json', function(error, lobbyistsContacts) {
     var n1 = graph.addNode(obj.Lobbyist_Firm);
 
     if(typeof n1 !== 'undefined') {
-      //n1.name = obj.Lobbyist_Firm;
       n1.group = 1;
     }
 
@@ -107,36 +154,35 @@ d3.json('data/lobbyistsContacts.json', function(error, lobbyistsContacts) {
     var n2 = graph.addNode(obj.Official_Department);
 
     if(typeof n2 !== 'undefined') {
-      //n2.name = obj.Official_Department;
       n2.group = 2;
     }
 
     /*
-      Date: "2014-11-30T00:00:00"
+      Date: '2014-11-30T00:00:00'
 
-      DesiredOutcome: "enforcement of prior settlement agreement; mitigation of construction impact on Store Operations in Union Square"
+      DesiredOutcome: 'enforcement of prior settlement agreement; mitigation of construction impact on Store Operations in Union Square'
 
-      FileNumber: "n/a"
+      FileNumber: 'n/a'
 
-      LobbyingSubjectArea: "Transportation"
+      LobbyingSubjectArea: 'Transportation'
 
-      Lobbyist: "Yaki, Michael"
-      Lobbyist_Client: "Barneys New York"
-      Lobbyist_Firm: "Michael Yaki"
+      Lobbyist: 'Yaki, Michael'
+      Lobbyist_Client: 'Barneys New York'
+      Lobbyist_Firm: 'Michael Yaki'
 
-      MunicipalDecision: "Construction Impact Of Central Subway"
+      MunicipalDecision: 'Construction Impact Of Central Subway'
 
-      Official: "Yuen, Janis"
-      Official_Department: "Municipal Transportation Agency"
+      Official: 'Yuen, Janis'
+      Official_Department: 'Municipal Transportation Agency'
 
       created_at: 1417625821
-      created_meta: "400501"
-      id: "C1C7C9DB-0108-4F5B-8915-7F4D89A632BA"
+      created_meta: '400501'
+      id: 'C1C7C9DB-0108-4F5B-8915-7F4D89A632BA'
       meta: null
       position: 262349
       sid: 262349
       updated_at: 1417625821
-      updated_meta: "400501"
+      updated_meta: '400501'
     */
   });
 
@@ -158,7 +204,7 @@ d3.json('data/lobbyistsContacts.json', function(error, lobbyistsContacts) {
       edge.weight++;
     }
     else {
-      graph.addEdge(fromId, toId);
+      var newEdge = graph.addEdge(fromId, toId);
     }
   });
 
@@ -183,7 +229,6 @@ d3.json('data/lobbyistsContacts.json', function(error, lobbyistsContacts) {
 
 
 
-
   force
       .nodes(nodes)
       .links(links)
@@ -192,30 +237,103 @@ d3.json('data/lobbyistsContacts.json', function(error, lobbyistsContacts) {
   var link = svg.selectAll('.link')
       .data(links)
     .enter().append('line')
-      .attr('class', 'link')
-      .style('stroke-width', function(d) { return Math.sqrt(d.value); });
+      .attr('class', 'link');
 
   var node = svg.selectAll('.node')
       .data(nodes)
-    .enter().append('circle')
+    .enter().append('g')
       .attr('class', 'node')
-      .attr('r', 5)
-      .style('fill', function(d) { return color(d.group); })
-      .call(force.drag);
+      .on('dblclick', dblclick)
+      .on('mouseover', function (d) { onMouseOver(d); })
+      .on('mouseout', function (d) { onMouseOut(d); })
+      .call(drag);
 
-  node.append('title')
+  node.append('circle')
+      .attr('r', function(d) {
+        d.radius = (d.weight < 5 ? 5 : d.weight);
+        return d.radius;
+      })
+      .style('fill', function(d) {
+        /*
+        if(d.group === 1) {
+          return '#27aae1';
+        }
+        else {
+          return '#231f20';
+        }
+        */
+        return color(d.group);
+      });
+
+
+  node.append('text')
+      .attr('class', 'label')
+      .attr('dx', 8) //change position based on circle size.
+      .attr('dy', '.35em')
+      .attr('opacity', 0)
       .text(function(d) { return d.name; });
 
-  force.on('tick', function() {
+
+  force.on('tick', function(e) {
+    var q = d3.geom.quadtree(nodes),
+        i = 0,
+        n = nodes.length;
+    while (++i < n) {
+      if(typeof nodes[i].fixed === 'undefined') {
+        q.visit(collide(nodes[i]));
+      }
+    }
+
     link.attr('x1', function(d) { return d.source.x; })
         .attr('y1', function(d) { return d.source.y; })
         .attr('x2', function(d) { return d.target.x; })
         .attr('y2', function(d) { return d.target.y; });
 
-    node.attr('cx', function(d) { return d.x; })
+    /*
+    var k = .1 * e.alpha;
+    nodes.forEach(function(o, i) {
+      //o.y += (foci[o.group-1].y - o.y) * k;
+      //o.x += (foci[o.group-1].x - o.x) * k;
+      o.y += i & 1 ? k : -k;
+      o.x += i & 2 ? k : -k;
+    });
+    */
+    node.selectAll('circle')
+        .attr('cx', function(d) { return d.x; })
         .attr('cy', function(d) { return d.y; });
+
+    node.selectAll('text')
+        .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+
+    //node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
   });
 
+
+
+  function collide(node) {
+    var r = node.radius + 64, //fish: was 16.
+        nx1 = node.x - r,
+        nx2 = node.x + r,
+        ny1 = node.y - r,
+        ny2 = node.y + r;
+
+    return function(quad, x1, y1, x2, y2) {
+      if (quad.point && (quad.point !== node)) {
+        var x = node.x - quad.point.x,
+            y = node.y - quad.point.y,
+            l = Math.sqrt(x * x + y * y),
+            r = node.radius + quad.point.radius;
+        if (l < r) {
+          l = (l - r) / l * .5;
+          node.x -= x *= l;
+          node.y -= y *= l;
+          quad.point.x += x;
+          quad.point.y += y;
+        }
+      }
+      return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+    };
+  }
 
 
 });
